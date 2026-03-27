eff40c749821a3164a393a42105516c609d1876e
whowho17 <sayammehta17@gmail.com>
2026-03-27 15:21:39 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "*.googleusercontent.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+    ],
+  },
+};
+
+export default nextConfig;
